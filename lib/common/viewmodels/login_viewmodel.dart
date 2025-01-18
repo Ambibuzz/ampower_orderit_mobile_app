7e0a26a130f9168e997e4c92ffdba21d8da4548a
mohitambi <mohit.yadav@ambibuzz.com>
2025-01-18 17:14:15 +0530

    added check for invalid site url
    Signed-off-by: Mohit Yadav <mohit.yadav@ambibuzz.com>

--- lib/common/viewmodels/login_viewmodel.dart
+++ lib/common/viewmodels/login_viewmodel.dart
@@ -1,3 +1,4 @@
+import 'package:dio/dio.dart';
 import 'package:orderit/locators/locator.dart';
 import 'package:orderit/common/services/login_api_service.dart';
 import 'package:orderit/common/services/storage_service.dart';
@@ -26,5 +27,27 @@ class LoginViewModel extends BaseViewModel {
   Future<String> getInstanceUrl() async {
     var instanceUrl = locator.get<StorageService>().apiUrl;
     return instanceUrl;
+  }
+
+  Future<bool> isUrlActive(String url) async {
+    final dio = Dio();
+    try {
+      // Use a HEAD request for efficiency
+      final response = await dio.head(url);
+
+      // Check if the status code is in the 2xx range (success)
+      return response.statusCode != null &&
+          response.statusCode! >= 200 &&
+          response.statusCode! < 300;
+    } on DioException catch (e) {
+      // Handle specific errors like timeouts, 404, etc.
+      if (e.response != null) {
+        return false; // URL exists but is not active
+      } else {
+        return false; // URL doesn't exist or can't be reached
+      }
+    } catch (e) {
+      return false;
+    }
   }
 }
