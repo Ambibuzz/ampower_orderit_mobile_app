4d1d6957913c9dc5f1cf5bf550aeddec2e68b918
mohitambi <mohit.yadav@ambibuzz.com>
2025-01-30 13:21:04 +0530

    added in app updates

--- lib/app.dart
+++ lib/app.dart
@@ -1,3 +1,6 @@
+import 'dart:io';
+
+import 'package:flutter_upgrade_version/flutter_upgrade_version.dart';
 import 'package:orderit/config/exception.dart';
 import 'package:orderit/locators/locator.dart';
 import 'package:orderit/app_viewmodel.dart';
@@ -15,54 +18,77 @@ import 'package:orderit/base_view.dart';
 import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
 import 'package:form_builder_validators/form_builder_validators.dart';
-import 'package:new_version_plus/new_version_plus.dart';
 import 'package:provider/provider.dart';
 import 'route/router.dart' as router;
 
-class App extends StatelessWidget {
+class App extends StatefulWidget {
   final bool? login;
   App({this.login, super.key});
   static var storageService = locator.get<StorageService>();
 
-  void basicStatusCheck(NewVersionPlus newVersion, BuildContext context) {
-    try {
-      if (!kDebugMode) {
-        newVersion.showAlertIfNecessary(context: context);
-      }
-    } catch (e) {
-      exception(e, '', 'basicStatusCheck');
-    }
+  @override
+  State<App> createState() => _AppState();
+}
+
+class _AppState extends State<App> {
+  var _packageInfo = PackageInfo();
+
+  @override
+  void initState() {
+    super.initState();
+    getPackageData();
   }
 
-  void advancedStatusCheck(
-      NewVersionPlus newVersion, BuildContext context) async {
-    try {
-      final status = await newVersion.getVersionStatus();
-      if (status != null) {
-        debugPrint(status.releaseNotes);
-        debugPrint(status.appStoreLink);
-        debugPrint(status.localVersion);
-        debugPrint(status.storeVersion);
-        debugPrint(status.canUpdate.toString());
-        newVersion.showUpdateDialog(
-          context: context,
-          versionStatus: status,
-          dialogTitle: 'Custom Title',
-          dialogText: 'Custom Text',
-        );
+  // Platform messages are asynchronous, so we initialize in an async method.
+  Future<void> getPackageData() async {
+    // Platform messages may fail, so we use a try/catch PlatformException.
+    // We also handle the message potentially returning null.
+    // If the widget was removed from the tree while the asynchronous platform
+    // message was in flight, we want to discard the reply rather than calling
+    // setState to update our non-existent appearance.
+    if (!mounted) return;
+    _packageInfo = await PackageManager.getPackageInfo();
+    // Locale myLocale = Localizations.localeOf(context);
+    // print('LOCALE: ${myLocale.languageCode} || ${myLocale.countryCode}');
+    if (Platform.isAndroid) {
+      var manager = InAppUpdateManager();
+      var appUpdateInfo = await manager.checkForUpdate();
+      if (appUpdateInfo == null) return;
+      if (appUpdateInfo.updateAvailability ==
+          UpdateAvailability.developerTriggeredUpdateInProgress) {
+        //If an in-app update is already running, resume the update.
+        var message =
+            await manager.startAnUpdate(type: AppUpdateType.immediate);
+        debugPrint(message ?? '');
+      } else if (appUpdateInfo.updateAvailability ==
+          UpdateAvailability.updateAvailable) {
+        ///Update available
+        if (appUpdateInfo.immediateAllowed) {
+          var message =
+              await manager.startAnUpdate(type: AppUpdateType.immediate);
+          debugPrint(message ?? '');
+        } else if (appUpdateInfo.flexibleAllowed) {
+          var message =
+              await manager.startAnUpdate(type: AppUpdateType.flexible);
+          debugPrint(message ?? '');
+        } else {
+          debugPrint(
+              'Update available. Immediate & Flexible Update Flow not allow');
+        }
       }
-    } catch (e) {
-      exception(e, '', 'advancedStatusCheck');
+    } else if (Platform.isIOS) {
+      var _versionInfo = await UpgradeVersion.getiOSStoreVersion(
+        packageInfo: _packageInfo,
+      );
+      debugPrint(_versionInfo.toJson().toString());
     }
+    setState(() {});
   }
 
   @override
   Widget build(BuildContext context) {
     return BaseView<AppViewModel>(
-      onModelReady: (model) async {
-        final newVersion = NewVersionPlus();
-        basicStatusCheck(newVersion, context);
-      },
+      onModelReady: (model) async {},
       builder: (context, model, child) {
         return LifeCycleManager(
           child: StreamProvider<ConnectivityStatus>(
@@ -103,8 +129,9 @@ class App extends StatelessWidget {
                     localizationsDelegates: const [
                       FormBuilderLocalizations.delegate,
                     ],
-                    initialRoute:
-                        (login == true ? splashViewRoute : (loginViewRoute)),
+                    initialRoute: (widget.login == true
+                        ? splashViewRoute
+                        : (loginViewRoute)),
                     debugShowCheckedModeBanner: false,
                     theme: theme,
                   );
