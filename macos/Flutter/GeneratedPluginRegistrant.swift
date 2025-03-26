20d7551db1b36e82247bf9f101d4a11b00e5be7b
mohitambi <mohit.yadav@ambibuzz.com>
2025-03-26 19:15:53 +0530

    compilation changed for ios
    Signed-off-by: mohitambi <mohit.yadav@ambibuzz.com>

--- macos/Flutter/GeneratedPluginRegistrant.swift
+++ macos/Flutter/GeneratedPluginRegistrant.swift
@@ -6,8 +6,8 @@ import FlutterMacOS
 import Foundation
 
 import connectivity_plus
+import file_picker
 import file_selector_macos
-import flutter_image_compress_macos
 import package_info_plus
 import path_provider_foundation
 import rive_common
@@ -18,8 +18,8 @@ import url_launcher_macos
 
 func RegisterGeneratedPlugins(registry: FlutterPluginRegistry) {
   ConnectivityPlugin.register(with: registry.registrar(forPlugin: "ConnectivityPlugin"))
+  FilePickerPlugin.register(with: registry.registrar(forPlugin: "FilePickerPlugin"))
   FileSelectorPlugin.register(with: registry.registrar(forPlugin: "FileSelectorPlugin"))
-  FlutterImageCompressMacosPlugin.register(with: registry.registrar(forPlugin: "FlutterImageCompressMacosPlugin"))
   FPPPackageInfoPlusPlugin.register(with: registry.registrar(forPlugin: "FPPPackageInfoPlusPlugin"))
   PathProviderPlugin.register(with: registry.registrar(forPlugin: "PathProviderPlugin"))
   RivePlugin.register(with: registry.registrar(forPlugin: "RivePlugin"))
