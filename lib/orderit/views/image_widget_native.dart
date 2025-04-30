6f5e0062f24e5a35b8c6adab81fdd3fd338e4aa8
mohitambi <mohit.yadav@ambibuzz.com>
2025-04-30 14:17:16 +0530

    added placeholder and error widget
    Signed-off-by: mohitambi <mohit.yadav@ambibuzz.com>

--- lib/orderit/views/image_widget_native.dart
+++ lib/orderit/views/image_widget_native.dart
@@ -1,18 +1,39 @@
 import 'dart:io';
 
 import 'package:cached_network_image/cached_network_image.dart';
+import 'package:flutter/foundation.dart';
 import 'package:orderit/config/styles.dart';
 import 'package:orderit/config/theme.dart';
+import 'package:orderit/util/constants/images.dart';
+import 'package:orderit/util/constants/sizes.dart';
 import 'package:orderit/util/dio_helper.dart';
 import 'package:flutter/material.dart';
+import '../../orderit/views/image_widget_native.dart'
+    if (dart.library.html) 'image_widget_web.dart' as image_widget;
 
 Widget imageWidget(String url, double? width, double? height, {BoxFit? fit}) {
   return CachedNetworkImage(
     imageUrl: url,
     width: width,
     height: height,
-    placeholder: (context, url) => const Icon(Icons.error),
-    errorWidget: (context, url, error) => const Icon(Icons.error),
+    placeholder: (context, url) => CircularProgressIndicator.adaptive(
+      padding: EdgeInsets.all(defaultTargetPlatform == TargetPlatform.android
+          ? Sizes.smallPaddingWidget(context)
+          : 0),
+    ),
+    errorWidget: (context, url, error) => Container(
+      width: width,
+      height: width,
+      decoration: const BoxDecoration(
+        borderRadius: Corners.lgBorder,
+        image: DecorationImage(
+          image: AssetImage(
+            Images.imageNotFound,
+          ),
+          fit: BoxFit.cover,
+        ),
+      ),
+    ),
     httpHeaders: {HttpHeaders.cookieHeader: DioHelper.cookies ?? ''},
   );
 }
