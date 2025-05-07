6d0f82d613742bc3989cb6eed01dd9a40848b93f
mohitambi <mohit.yadav@ambibuzz.com>
2025-05-07 15:03:45 +0530

    improved ui of typeahead field widget
    Signed-off-by: mohitambi <mohit.yadav@ambibuzz.com>

--- lib/common/widgets/typeahead_widgets.dart
+++ lib/common/widgets/typeahead_widgets.dart
@@ -1,15 +1,28 @@
 import 'package:flutter/material.dart';
+import 'package:orderit/util/constants/sizes.dart';
+import 'package:orderit/util/display_helper.dart';
 
 class TypeAheadWidgets {
   static Widget itemUi(String item, BuildContext context, {Color? textColor}) {
-    return ListTile(
-      key: Key(item),
-      title: Text(
-        item,
-        style: Theme.of(context).textTheme.titleMedium?.copyWith(
-              color: textColor,
-            ),
-      ),
+    return Column(
+      mainAxisAlignment: MainAxisAlignment.center,
+      crossAxisAlignment: CrossAxisAlignment.start,
+      children: [
+        Padding(
+          padding: EdgeInsets.symmetric(
+            vertical: Sizes.smallPaddingWidget(context) * 1.5,
+            horizontal: displayWidth(context) < 600 ? 16 : 24,
+          ),
+          child: Text(
+            key: Key(item),
+            item,
+            style: Theme.of(context).textTheme.titleSmall?.copyWith(
+                  color: textColor,
+                  fontWeight: FontWeight.w500,
+                ),
+          ),
+        ),
+      ],
     );
   }
 
